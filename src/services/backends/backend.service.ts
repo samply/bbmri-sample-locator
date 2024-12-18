41d0321d31dc225bde608243003d10ea2f3f3d9a
Enola Knezevic <115070135+enola-dkfz@users.noreply.github.com>
2024-12-18 16:57:43 +0100

    Added Rome-OPBG and Uppsala display names

--- src/services/backends/backend.service.ts
+++ src/services/backends/backend.service.ts
@@ -34,33 +34,36 @@ export const requestBackend = (ast: AstTopLayer, updateResponse: (response: Map<
 
     let backendUrl: string = "";
 
-    /**
-     * TODO: add different backend URLs for different environments
-     */
-
-    // if (import.meta.env.VITE_TARGET_ENVIRONMENT === "production") {
-    //     backendUrl = "https://locator-dev.bbmri-eric.eu/backend";
-    // } else if (import.meta.env.VITE_TARGET_ENVIRONMENT === "staging") {
+    if (import.meta.env.VITE_TARGET_ENVIRONMENT === "production") {
         backendUrl = "https://locator.bbmri-eric.eu/backend/";
-    // } else {
-    //     backendUrl = "http://localhost:8055";
-    // }
+    } else if (import.meta.env.VITE_TARGET_ENVIRONMENT === "staging") {
+        backendUrl = "https://locator.bbmri-eric.eu/backend/";
+        // TODO: Turn this back on after merging to main
+        // backendUrl = "https://locator-dev.bbmri-eric.eu/backend";
+    } else {
+        backendUrl = "http://localhost:8055";
+    }
 
     const backend = new Spot(new URL(backendUrl), [
         "aachen",
+        "augsburg",
         "berlin",
         "brno",
         "brno-recetox",
         "cyprus",
         "dresden",
+        "essen",
         "frankfurt",
         "goettingen",
         "hannover",
         "heidelberg",
+        "leipzig",
         "luebeck",
         "mannheim",
         "marburg",
         "muenchen-hmgu",
+        "muenster",
+        "naples-pascale",
         "olomouc",
         "pilsen",
         "prague-ffm",
@@ -72,6 +75,7 @@ export const requestBackend = (ast: AstTopLayer, updateResponse: (response: Map<
         "wuerzburg",
     ], queryId);
 
+    // TODO: Reactivate for test environment
     // const backend = new Spot(new URL(backendUrl), [
     //     "uppsala-test",
     //     "eric-test",
