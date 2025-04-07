e9b5653d45acca06298c389b6959f7e0edd23a3d
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-04-07 10:55:53 +0200

    chore: fix options path

--- scripts/validate-json-schema.bash
+++ scripts/validate-json-schema.bash
@@ -1,5 +1,5 @@
 set -e # Return non-zero exit status if one of the validations fails
-npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/options.schema.json -d static/options.json
-npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/options.schema.json -d static/options-test.json
-npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/options.schema.json -d static/options-acceptance.json
+npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/options.schema.json -d static/config/options.json
+npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/options.schema.json -d static/config/options-test.json
+npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/options.schema.json -d static/config/options-acceptance.json
 npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/catalogue.schema.json -d static/catalogues/catalogue-bbmri.json
