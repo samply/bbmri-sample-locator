ec149a83b8413028d25f8e7cdda039bcdff0de12
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-04-07 10:38:37 +0200

    chore: validate options-acceptance.json in CI

--- scripts/validate-json-schema.bash
+++ scripts/validate-json-schema.bash
@@ -1,4 +1,5 @@
 set -e # Return non-zero exit status if one of the validations fails
 npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/options.schema.json -d static/options.json
 npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/options.schema.json -d static/options-test.json
+npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/options.schema.json -d static/options-acceptance.json
 npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/catalogue.schema.json -d static/catalogues/catalogue-bbmri.json
